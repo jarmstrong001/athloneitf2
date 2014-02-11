deddf534fad40f99bc41db41a6990f66f291711a
jarmstrong001 <joss.armstrong@gmail.com>
2014-02-11 17:09:20 +0000

    a

---
+++ athloneitf2/src/com/athloneitf/datatype/Payment.java
@@ -0,0 +1,55 @@
+package com.athloneitf.datatype;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+public class Payment {
+
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column
+	private int paymentId;
+	@Column
+	private int memberCode;
+	@Column
+	private Date paymentDate;
+	@Column
+	private Date paymentFrom;
+	@Column
+	private double paymentAmount;
+	
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public int getMemberCode() {
+		return memberCode;
+	}
+	public void setMemberCode(int memberCode) {
+		this.memberCode = memberCode;
+	}
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	public Date getPaymentFrom() {
+		return paymentFrom;
+	}
+	public void setPaymentFrom(Date paymentFrom) {
+		this.paymentFrom = paymentFrom;
+	}
+	public double getPaymentAmount() {
+		return paymentAmount;
+	}
+	public void setPaymentAmount(double paymentAmount) {
+		this.paymentAmount = paymentAmount;
+	}
+	
+}
